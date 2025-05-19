9eb1f093ad8000be026445c2a65d7ef6a01d8f42
Rebecca Black <rebecca.ann.black@gmail.com>
2025-05-19 14:56:13 -0400

    removing search icon from header

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -28,13 +28,13 @@
       <li><a href={base + '/Team'}>Team</a></li>
     </ul>
 
-
-    <button class="search-icon" aria-label="Search">
+    <!-- hiding serach icon for now since search isn't implemented yet -->
+    <!-- <button class="search-icon" aria-label="Search">
       <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="24" height="24">
         <circle cx="11" cy="11" r="8" />
         <line x1="21" y1="21" x2="16.65" y2="16.65" />
       </svg>
-    </button>
+    </button> -->
   </div>
 </header>
 
