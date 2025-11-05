c2e5d0a80f89b932aa31a7bdbceb1d755824f811
Akosua <akessah@mit.edu>
2025-11-05 20:26:47 +0000

    added dutch data 1748-1798

--- src/data/dutch-performances.csv.js
+++ src/data/dutch-performances.csv.js
@@ -4,7 +4,7 @@ import {csvFormat} from "d3-dsv";
 import {readFile} from "fs";
 import {parse} from "csv-parse/sync";
 
-readFile("src/data/dutch_data.csv", "utf8", (err, data) => {
+readFile("src/data/dutch_data_1748-1798.csv", "utf8", (err, data) => {
     const parsed = parse(data, { columns: true });
     const transformed = parsed.map((record) => ({
         year: (new Date(record.date)).getFullYear(),
